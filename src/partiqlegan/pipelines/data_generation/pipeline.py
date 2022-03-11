da8a92d30adf5b226bccf340334f539114398650
lc3267 <melvin.strobl@kit.edu>
2022-03-11 09:08:43 +0100

    node to pipeline

--- src/partiqlegan/pipelines/data_generation/pipeline.py
+++ src/partiqlegan/pipelines/data_generation/pipeline.py
@@ -4,7 +4,14 @@ generated using Kedro 0.17.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-
+from .nodes import gen_nbody_decay_data
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+                func=gen_nbody_decay_data,
+                inputs="phasespace",
+                outputs="nbody_decay_data",
+                name="gen_nbody_decay_data",
+            )
+    ])
