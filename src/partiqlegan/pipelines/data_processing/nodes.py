27ab026eef2905ce9feb09385202c96abf9cd24d
lc3267 <melvin.strobl@kit.edu>
2022-03-15 09:26:37 +0100

    dummy nodes

--- src/partiqlegan/pipelines/data_processing/nodes.py
+++ src/partiqlegan/pipelines/data_processing/nodes.py
@@ -2,3 +2,14 @@
 This is a boilerplate pipeline 'data_processing'
 generated using Kedro 0.17.7
 """
+import numpy as np
+from typing import Dict, Tuple
+import torchdata
+
+def train_test_split(
+    intermediate_tree: Dict[str, np.ndarray]
+) -> Tuple[np.ndarray, np.ndarray]:
+
+
+
+    return 
--- src/partiqlegan/pipelines/data_science/nodes.py
+++ src/partiqlegan/pipelines/data_science/nodes.py
@@ -3,3 +3,13 @@ This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.17.7
 """
 
+import numpy as np
+from typing import Dict, Tuple
+from phasespace import GenParticle, nbody_decay
+from phasespace.fromdecay import GenMultiDecay
+import pandas as pd
+
+def processingDummy(
+    intermediate_tree: Dict[str, np.ndarray]
+) -> Dict[str, np.ndarray]:
+    return 
