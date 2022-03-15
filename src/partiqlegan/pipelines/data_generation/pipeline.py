33f12a9a9207eb809b2e8a217d7df50cfdb1ab90
lc3267 <melvin.strobl@kit.edu>
2022-03-15 09:26:25 +0100

    alternative decay gen node import

--- src/partiqlegan/pipelines/data_generation/pipeline.py
+++ src/partiqlegan/pipelines/data_generation/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.17.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import gen_nbody_decay_data
+from .nodes import gen_nbody_decay_data, gen_decay_from_file
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
@@ -13,5 +13,11 @@ def create_pipeline(**kwargs) -> Pipeline:
                 inputs="phasespace",
                 outputs="nbody_decay_data",
                 name="gen_nbody_decay_data",
+            ),
+        node(
+                func=gen_decay_from_file,
+                inputs="decaylanguage",
+                outputs="decay_tree_data",
+                name="gen_decay_from_file",
             )
     ])
