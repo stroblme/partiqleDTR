adcf42315dff4f47c1df4f920ca06b2a712688d9
lc3267 <melvin.strobl@kit.edu>
2022-04-26 16:18:00 +0200

    piplines for data processing nodes

--- src/partiqlegan/pipelines/data_processing/pipeline.py
+++ src/partiqlegan/pipelines/data_processing/pipeline.py
@@ -4,7 +4,32 @@ generated using Kedro 0.17.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-
+from .nodes import tree_data_to_generator, tree_data_to_discriminator, normalize
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+                func=tree_data_to_generator,
+                inputs="decay_tree_events",
+                outputs="generator_input",
+                name="tree_data_to_generator"
+        ),
+        node(
+                func=tree_data_to_discriminator,
+                inputs="decay_tree_structure",
+                outputs="discriminator_input",
+                name="tree_data_to_discriminator"
+        ),
+        node(
+                func=normalize,
+                inputs="generator_input",
+                outputs="generator_input_normalized",
+                name="normalize_generator"
+        ),
+        node(
+                func=normalize,
+                inputs="discriminator_input",
+                outputs="discriminator_input_normalized",
+                name="normalize_discriminator"
+        )
+    ])
