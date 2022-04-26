93740141bc0bd10385c4791064d73b47d6025cdb
lc3267 <melvin.strobl@kit.edu>
2022-04-26 16:13:48 +0200

    data generation in pipeline

--- src/partiqlegan/pipelines/data_generation/pipeline.py
+++ src/partiqlegan/pipelines/data_generation/pipeline.py
@@ -4,20 +4,23 @@ generated using Kedro 0.17.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import gen_nbody_decay_data, gen_decay_from_file
+from .nodes import gen_decay_from_file #, gen_nbody_decay_data
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
-        node(
-                func=gen_nbody_decay_data,
-                inputs="phasespace",
-                outputs="nbody_decay_data",
-                name="gen_nbody_decay_data",
-            ),
+        # node(
+        #         func=gen_nbody_decay_data,
+        #         inputs="phasespace",
+        #         outputs="nbody_decay_data",
+        #         name="gen_nbody_decay_data",
+        #     ),
         node(
                 func=gen_decay_from_file,
                 inputs="decaylanguage",
-                outputs="decay_tree_data",
+                outputs={
+                    "decay_tree_structure":"decay_tree_structure",
+                    "decay_tree_events":"decay_tree_events"
+                },
                 name="gen_decay_from_file",
             )
     ])
