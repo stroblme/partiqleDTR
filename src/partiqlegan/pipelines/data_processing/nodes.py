2e800f41240a2eae22108553377c719fc035494d
lc3267 <melvin.strobl@kit.edu>
2022-04-26 16:14:08 +0200

    started with data pre processing

--- src/partiqlegan/pipelines/data_processing/nodes.py
+++ src/partiqlegan/pipelines/data_processing/nodes.py
@@ -3,7 +3,7 @@ This is a boilerplate pipeline 'data_processing'
 generated using Kedro 0.17.7
 """
 import numpy as np
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 
 import torch
 import torchvision
@@ -20,26 +20,73 @@ data_transform = torchvision.transforms.Compose(
     ]
 )
 
-class DecayTreeDataset(torch.utils.data.dataset):
-    def __init__(self, dataset:Dict[str, np.ndarray]):
+# class DecayTreeDataset(torch.utils.data.dataset):
+#     def __init__(self, dataset:Dict[str, np.ndarray]):
 
-        self.decayTreeDataset = dataset
+#         self.decayTreeDataset = dataset
         
-        raise NotImplementedError("Sorry, not yet..")
+#         raise NotImplementedError("Sorry, not yet..")
 
-    def __len__(self):
-        return len(self.decayTreeDataset)
+#     def __len__(self):
+#         return len(self.decayTreeDataset)
 
-    def __getitem__(self, idx):
-        if torch.is_tensor(idx):
-            idx = idx.tolist()
+#     def __getitem__(self, idx):
+#         if torch.is_tensor(idx):
+#             idx = idx.tolist()
 
-        raise NotImplementedError("Sorry, not yet..")
+#         raise NotImplementedError("Sorry, not yet..")
         
-def train_test_split(
-    intermediate_tree: Dict[str, np.ndarray]
-) -> Tuple[np.ndarray, np.ndarray]:
+allowed_fsps = ["e+", "e-", "pi+", "pi-", "gamma"]
+
+def tree_data_to_discriminator(
+    decay_tree_structure: Tuple[List, List]
+) -> Dict[str, np.ndarray]:
+
+    # raise NotImplementedError("Sorry, not yet..")
+    nodes = list()
+    probabilities = list()
+
+    def add_node(name):
+        if name in allowed_fsps:
+            nodes.append(name)
 
 
+    def get_nodes(tree):
+        if type(tree) == str:
+            add_node(tree)
 
-    return 
+        elif type(tree) == list:
+            for sub_tree in tree:
+                get_nodes(sub_tree)
+                
+
+        elif type(tree) == dict:
+            for key, value in tree.items():
+                # fsps
+                if key == "fs":
+                    get_nodes(value)
+                # probabilities
+                elif key == "bf":
+                    probabilities.append(value)
+                # 1st gen particles
+                else:
+                    add_node(key)
+                    get_nodes(value)
+
+    get_nodes(decay_tree_structure)
+    return decay_tree_structure
+
+def tree_data_to_generator(
+    decay_tree_events: Dict
+) -> Dict[str, np.ndarray]:
+
+    # raise NotImplementedError("Sorry, not yet..")
+
+    return decay_tree_events
+
+
+def normalize(
+    data: np.ndarray
+) -> np.ndarray:
+
+    return data
