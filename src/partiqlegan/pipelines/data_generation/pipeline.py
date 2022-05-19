73c877ae11d85d7c00c6e90b8e47eb5ea0d6caa8
lc3267 <melvin.strobl@kit.edu>
2022-05-19 10:43:51 +0200

    renamed node

--- src/partiqlegan/pipelines/data_generation/pipeline.py
+++ src/partiqlegan/pipelines/data_generation/pipeline.py
@@ -4,19 +4,19 @@ generated using Kedro 0.17.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import gen_decay_from_file #, gen_nbody_decay_data
+from .nodes import gen_decay_from_file, gen_nbody_decay_data
 
 def create_pipeline(**kwargs) -> Pipeline:
     return pipeline([
-        # node(
-        #         func=gen_nbody_decay_data,
-        #         inputs="phasespace",
-        #         outputs="nbody_decay_data",
-        #         name="gen_nbody_decay_data",
-        #     ),
+        node(
+                func=gen_nbody_decay_data,
+                inputs="simple_decay",
+                outputs="nbody_decay_data",
+                name="gen_nbody_decay_data",
+            ),
         node(
                 func=gen_decay_from_file,
-                inputs="decaylanguage",
+                inputs="omega_decay",
                 outputs={
                     "decay_tree_structure":"decay_tree_structure",
                     "decay_tree_events":"decay_tree_events"
