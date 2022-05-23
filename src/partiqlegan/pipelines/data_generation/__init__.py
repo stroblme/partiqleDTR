099aee99f410ff82785786d355a3d9e24bd9373a
lc3267 <melvin.strobl@kit.edu>
2022-05-23 11:37:33 +0200

    data generation pipelines added

--- src/partiqlegan/pipelines/data_generation/__init__.py
+++ src/partiqlegan/pipelines/data_generation/__init__.py
@@ -3,6 +3,6 @@ This is a boilerplate pipeline 'data_generation'
 generated using Kedro 0.17.7
 """
 
-from .pipeline import create_pipeline
+from .pipeline import create_artificial_pipeline, create_belleII_pipeline
 
-__all__ = ["create_pipeline"]
+__all__ = ["create_artificial_pipeline", "create_belleII_pipeline"]
