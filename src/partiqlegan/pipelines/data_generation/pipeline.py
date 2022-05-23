e8eba62ebeaee1041b3a43dceee15367c79ff4cb
lc3267 <melvin.strobl@kit.edu>
2022-05-23 11:52:18 +0200

    renamed

--- src/partiqlegan/pipelines/data_generation/pipeline.py
+++ src/partiqlegan/pipelines/data_generation/pipeline.py
@@ -4,7 +4,7 @@ generated using Kedro 0.17.7
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
-from .nodes import gen_decay_from_file, gen_structure_from_parameters, gen_events_from_topologies
+from .nodes import gen_decay_from_file, gen_structure_from_parameters, gen_events_from_structure
 
 def create_artificial_pipeline(**kwargs) -> Pipeline:
     return pipeline([
@@ -17,12 +17,12 @@ def create_artificial_pipeline(**kwargs) -> Pipeline:
                 name="gen_structure_from_parameters",
         ),
         node(
-                func=gen_events_from_topologies,
+                func=gen_events_from_structure,
                 inputs=["artificial_decay", "decay_tree_structure"],
                 outputs={
                     "decay_tree_events":"decay_tree_events"
                 },
-                name="gen_events_from_topologies",
+                name="gen_events_from_structure",
         )
     ])
 
