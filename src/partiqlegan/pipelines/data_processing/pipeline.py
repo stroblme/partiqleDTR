b2aba969711c08ba47d0db740f6943e828206d20
lc3267 <melvin.strobl@kit.edu>
2022-05-25 10:25:09 +0200

    normalize for belleII removed

--- src/partiqlegan/pipelines/data_processing/pipeline.py
+++ src/partiqlegan/pipelines/data_processing/pipeline.py
@@ -5,7 +5,7 @@ generated using Kedro 0.17.7
 
 from kedro.pipeline import Pipeline, node
 from kedro.pipeline.modular_pipeline import pipeline
-from .nodes import tree_data_to_generator, tree_data_to_discriminator, normalize, conv_structure_to_lca_and_names, shuffle_lca_and_leaves, conv_to_gnn_input
+from .nodes import tree_data_to_generator, tree_data_to_discriminator, normalize_event, conv_structure_to_lca_and_names, shuffle_lca_and_leaves, normalize_event
 
 def create_belleII_pipeline(**kwargs) -> Pipeline:
     return pipeline(
@@ -22,15 +22,15 @@ def create_belleII_pipeline(**kwargs) -> Pipeline:
                     outputs="discriminator_input",
                     name="tree_data_to_discriminator"
             ),
-            node(
-                    func=normalize,
-                    inputs="generator_input",
-                    outputs="generator_input_normalized",
-                    name="normalize_generator"
-            ),
+            # node(
+            #         func=normalize_event,
+            #         inputs="generator_input",
+            #         outputs="generator_input_normalized",
+            #         name="normalize_generator"
+            # ),
             node(
                     func=conv_structure_to_lca_and_names,
-                    inputs="decay_tree_structure",
+                    inputs=["artificial_decay", "decay_tree_structure"],
                     outputs={
                         "all_lca":"all_lca",
                         "all_names":"all_names"
@@ -53,7 +53,7 @@ def create_artificial_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
                 func=conv_structure_to_lca_and_names,
-                inputs="decay_tree_structure",
+                inputs=["artificial_decay", "decay_tree_structure"],
                 outputs={
                     "all_lca":"all_lca",
                     "all_names":"all_names"
@@ -70,14 +70,19 @@ def create_artificial_pipeline(**kwargs) -> Pipeline:
                 name="shuffle_lca_and_leaves"
         ),
         node(
-                func=conv_to_gnn_input,
-                inputs=["model_parameters", "all_lca_shuffled", "all_leaves_shuffled"],
+                func=normalize_event,
+                inputs=["artificial_decay", "all_leaves_shuffled"],
                 outputs={
-                    "gnn_x":"gnn_x",
-                    "gnn_r_i":"gnn_r_i",
-                    "gnn_r_o":"gnn_r_o",
-                    "gnn_y":"gnn_y"
+                    "all_leaves_shuffled_normalized":"all_leaves_shuffled_normalized"
                 },
-                name="conv_to_gnn_input"
-        )
+                name="normalize_event"
+        ),
+        # node(
+        #         func=pad_lca,
+        #         inputs=["artificial_decay", "all_lca_shuffled"],
+        #         outputs={
+        #             "all_lca_shuffled_padded":"all_lca_padded",
+        #         },
+        #         name="pad_lca"
+        # )
     ])
