1db709c712eed3b4d8b7e2eece6cf8e80481fc31
lc3267 <melvin.strobl@kit.edu>
2022-05-27 10:51:37 +0200

    added nri spec models and instructors

--- src/partiqlegan/pipelines/data_science/__init__.py
+++ src/partiqlegan/pipelines/data_science/__init__.py
@@ -3,6 +3,8 @@ This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.17.7
 """
 
+from .instructors import *
+from .models import *
 from .pipeline import create_pipeline
 
 __all__ = ["create_pipeline"]
--- src/partiqlegan/pipelines/data_science/pipeline.py
+++ src/partiqlegan/pipelines/data_science/pipeline.py
@@ -5,6 +5,17 @@ generated using Kedro 0.17.7
 
 from kedro.pipeline import Pipeline, node, pipeline
 
+from .nodes_bak import train_qgnn
+
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline([
+        node(
+                func=train_qgnn,
+                inputs=["model_parameters", "all_leaves_shuffled", "all_lca_shuffled"],
+                outputs={
+                    "model":"model"
+                },
+                name="train_qgnn"
+        )
+    ])
