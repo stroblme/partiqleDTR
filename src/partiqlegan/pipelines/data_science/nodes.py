4b708df3344d477d901a3a84226779d2c7432929
lc3267 <melvin.strobl@kit.edu>
2022-06-01 14:09:48 +0200

    simplified and data preprocessing

--- src/partiqlegan/pipelines/data_science/nodes.py
+++ src/partiqlegan/pipelines/data_science/nodes.py
@@ -1,11 +1,15 @@
 import torch
-from instructors.XNRI_enc import XNRIENCIns
-from argparse import ArgumentParser
-from models.encoder import AttENC, RNNENC, GNNENC
-from models.decoder import GNNDEC, RNNDEC, AttDEC
-from models.nri import NRIModel
+from .instructors.XNRI_enc import XNRIENCIns
+from .models.encoder import GNNENC
+from .models.decoder import GNNDEC
+from .models.nri import NRIModel
 from torch.nn.parallel import DataParallel
-from generate.load import load_nri
+# from generate.load import load_nri
+from itertools import permutations
+import numpy as np
+from torch import LongTensor, FloatTensor
+
+from torch.utils.data.dataset import TensorDataset
 
 
 def train_qgnn(model_parameters, all_leaves_shuffled, all_lca_shuffled):
@@ -15,25 +19,29 @@ def train_qgnn(model_parameters, all_leaves_shuffled, all_lca_shuffled):
     REDUCE = model_parameters["REDUCE"] if "REDUCE" in model_parameters else None
     SKIP = model_parameters["SKIP"] if "SKIP" in model_parameters else None
     N_HID = model_parameters["N_HID"] if "N_HID" in model_parameters else None
+    DIM = model_parameters["DIM"] if "DIM" in model_parameters else None
 
 
-    num_of_leaves = all_lca_shuffled["train"][0][0].shape[0]
+    # data, es, _ = load_nri(all_leaves_shuffled, num_of_leaves)
+    # generate edge list of a fully connected graph
+    es = LongTensor(np.array(list(permutations(range(SIZE), 2))).T)
 
-    data, es, _ = load_nri(all_leaves_shuffled, num_of_leaves)
-    dim = 4
-    encs = {
-        'GNNENC': GNNENC,
-        'RNNENC': RNNENC,
-        'AttENC': AttENC,
-    }
-    decs = {
-        'GNNDEC': GNNDEC,
-        'RNNDEC': RNNDEC,
-        'AttDEC': AttDEC,
-    }
-    encoder = GNNENC(dim, N_HID, EDGE_TYPE, reducer=REDUCE)
-    decoder = GNNDEC(dim, EDGE_TYPE, N_HID, N_HID, N_HID, skip_first=SKIP)
+    # dim = 4
+
+    modes = all_lca_shuffled.keys()
+    data = dict()
+    for mode in modes:
+        x_data = []
+        y_data = []
+        for topology_it in range(len(all_lca_shuffled[mode])):
+            for i in range(len(all_lca_shuffled[mode][topology_it])):
+                x_data.append(all_leaves_shuffled[mode][topology_it][i])
+                y_data.append(all_lca_shuffled[mode][topology_it][i])
+        data[mode] = (LongTensor(y_data), FloatTensor(x_data))
+
+    encoder = GNNENC(DIM, N_HID, EDGE_TYPE, reducer=REDUCE)
+    decoder = GNNDEC(DIM, EDGE_TYPE, N_HID, N_HID, N_HID, skip_first=SKIP)
     model = NRIModel(encoder, decoder, es, SIZE)
     model = DataParallel(model)
-    ins = XNRIENCIns(model, data, es, model_parameters)
+    ins = XNRIENCIns(model_parameters, model, data, es, model_parameters)
     ins.train()
