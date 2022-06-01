ea1d0469979a6c8e80ee3ec9fe88783f0316d7e2
lc3267 <melvin.strobl@kit.edu>
2022-06-01 14:10:08 +0200

    utils to import

--- src/partiqlegan/pipelines/data_science/__init__.py
+++ src/partiqlegan/pipelines/data_science/__init__.py
@@ -4,7 +4,8 @@ generated using Kedro 0.17.7
 """
 
 from .instructors import *
+from .utils import *
 from .models import *
-from .pipeline import create_pipeline
+from .pipeline import create_training_qgnn_pipeline
 
 __all__ = ["create_pipeline"]
