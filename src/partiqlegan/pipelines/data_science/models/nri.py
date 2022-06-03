4559d481598fe6d9f08b6d6f8cc2a74d5718ec98
lc3267 <melvin.strobl@kit.edu>
2022-06-03 09:50:12 +0200

    nri cleanup

--- src/partiqlegan/pipelines/data_science/models/nri.py
+++ src/partiqlegan/pipelines/data_science/models/nri.py
@@ -6,7 +6,7 @@ import torch
 
 class NRIModel(nn.Module):
     """Auto-encoder."""
-    def __init__(self, encoder: nn.Module, decoder: nn.Module, es: Tensor, size: int):
+    def __init__(self, encoder: nn.Module, es: Tensor, size: int):
         """
         Args:
             encoder: an encoder inferring relations
@@ -16,7 +16,6 @@ class NRIModel(nn.Module):
         """
         super(NRIModel, self).__init__()
         self.enc = encoder
-        self.dec = decoder
         self.es = es
         self.size = size
 
@@ -36,46 +35,3 @@ class NRIModel(nn.Module):
         prob = logits.softmax(-1)
         return prob
 
-    def predict_states(self, states: Tensor, edges: Tensor, M: int=1) -> Tensor:
-        """
-        Given historical node states and inferred relations, predict future node states.
-
-        Args:
-            states: [batch, step, node, dim]
-            edges: [E, batch, K]
-            M: number of steps to predict
-
-        Return:
-            states: [batch, step_out, node, dim]
-        """
-        if not self.es.is_cuda:
-            self.es = self.es.cuda(states.device)
-        return self.dec(states, edges, self.es, M)
-
-    def forward(self, states_enc: Tensor, states_dec: Tensor, hard: bool=False, p: bool=False, M: int=1, tosym: bool=False, temp: float=0.5):
-        """
-        Args:
-            states_enc: [batch, step_enc, node, dim], input node states for the encoder
-            states_dec: [batch, step_dec, node, dim], input node states for the decoder
-            hard: predict one-hot representation of relations or its continuous relaxation, default: False
-            p: return the distribution of relations or not, default: True
-            M: number of steps to predict
-            tosym: impose hard constraint to inferred relations or not, default: False
-
-        Return:
-            output: [batch, step, node, dim]
-            prob (optioanl): [E, batch, K]
-        """
-        if not self.es.is_cuda:
-            self.es = self.es.cuda(states_enc.device)
-        logits = self.enc(states_enc, self.es)
-        if tosym:
-            logits = sym_hard(logits, self.size)
-        edges = gumbel_softmax(logits, tau=temp, hard=hard)
-        output = self.dec(states_dec, edges, self.es, M)
-        if p:
-            prob = logits.softmax(-1)
-            prob = prob.transpose(0, 1).contiguous()
-            return output, prob
-        else:
-            return output
