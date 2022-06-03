2a1037276def448216fce065fd97aeef04dcbc29
lc3267 <melvin.strobl@kit.edu>
2022-06-03 09:49:57 +0200

    gc

--- src/partiqlegan/pipelines/data_science/models/gnn.py
+++ src/partiqlegan/pipelines/data_science/models/gnn.py
@@ -78,7 +78,7 @@ class GNN(nn.Module):
             raise ValueError('i2o or o2i')
         x_i, x_o = x[row], x[col]
         z = self.node2edge(x_i, x_o, f_e)
-        return z, col, len(x)
+        return z
 
     def update(self, x):
         return x
