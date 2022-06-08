cd8317c83d317d8d3bb94d61e79bad4ca3f38979
lc3267 <melvin.strobl@kit.edu>
2022-06-08 10:37:50 +0200

    removed instructors import

--- src/partiqlegan/pipelines/data_science/__init__.py
+++ src/partiqlegan/pipelines/data_science/__init__.py
@@ -3,7 +3,6 @@ This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.17.7
 """
 
-from .instructors import *
 from .utils import *
 from .models import *
 from .pipeline import create_training_qgnn_pipeline
