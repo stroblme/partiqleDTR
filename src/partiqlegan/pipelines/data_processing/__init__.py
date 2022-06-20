d03a57f5d050d7a3aebc1cbf24c51f98d85fffda
lc3267 <melvin.strobl@kit.edu>
2022-06-20 14:48:44 +0200

    no shuffle in init

--- src/partiqlegan/pipelines/data_processing/__init__.py
+++ src/partiqlegan/pipelines/data_processing/__init__.py
@@ -3,6 +3,6 @@ This is a boilerplate pipeline 'data_processing'
 generated using Kedro 0.17.7
 """
 
-from .pipeline import create_belleII_pipeline, create_artificial_pipeline
+from .pipeline import create_belleII_pipeline, create_artificial_pipeline, create_artificial_pipeline_no_shuffle
 
-__all__ = ["create_belleII_pipeline", "create_artificial_pipeline"]
+__all__ = ["create_belleII_pipeline", "create_artificial_pipeline", "create_artificial_pipeline_no_shuffle"]
