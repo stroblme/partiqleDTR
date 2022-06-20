35cc5acc56662dee1cffe2306ae4d85e8b09d607
lc3267 <melvin.strobl@kit.edu>
2022-06-20 14:15:19 +0200

    adapted to mode sorting and shuffling

--- src/partiqlegan/pipelines/data_processing/pipeline.py
+++ src/partiqlegan/pipelines/data_processing/pipeline.py
@@ -61,28 +61,37 @@ def create_artificial_pipeline(**kwargs) -> Pipeline:
                 name="conv_structure_to_lca_and_names"
         ),
         node(
-                func=shuffle_lca_and_leaves,
+                func=lca_and_leaves_sort_into_modes,
                 inputs=["artificial_decay", "all_lca", "all_names", "decay_tree_events"],
+                outputs={
+                    "all_lca_mode_sorted":"all_lca_mode_sorted",
+                    "all_leaves_mode_sorted":"all_leaves_mode_sorted"
+                },
+                name="lca_and_leaves_sort_into_modes"
+        ),
+        node(
+                func=shuffle_lca_and_leaves_in_mode,
+                inputs=["artificial_decay", "all_lca_mode_sorted", "all_leaves_mode_sorted"],
                 outputs={
                     "all_lca_shuffled":"all_lca_shuffled",
                     "all_leaves_shuffled":"all_leaves_shuffled"
                 },
-                name="shuffle_lca_and_leaves"
+                name="shuffle_lca_and_leaves_in_mode"
         ),
         node(
-                func=merge_topologies,
+                func=lca_and_leaves_to_tuple_dataset,
                 inputs=["all_lca_shuffled", "all_leaves_shuffled"],
                 outputs={
                     "dataset_lca_and_leaves":"dataset_lca_and_leaves"
                 },
-                name="merge_topologies"
+                name="lca_and_leaves_to_tuple_dataset"
         ),
         node(
-                func=tuple_dataset_to_torch_tensor,
+                func=tuple_dataset_to_torch_tensor_dataset,
                 inputs=["dataset_lca_and_leaves"],
                 outputs={
                     "torch_dataset_lca_and_leaves":"torch_dataset_lca_and_leaves",
                 },
-                name="tuple_dataset_to_torch_tensor"
+                name="tuple_dataset_to_torch_tensor_dataset"
         )
     ])
