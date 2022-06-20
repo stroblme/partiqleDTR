ffb5d89395a87e6970448dcca9d3219bbf029371
lc3267 <melvin.strobl@kit.edu>
2022-06-20 14:15:36 +0200

    added no shuffle pipeline

--- src/partiqlegan/pipelines/data_processing/pipeline.py
+++ src/partiqlegan/pipelines/data_processing/pipeline.py
@@ -95,3 +95,50 @@ def create_artificial_pipeline(**kwargs) -> Pipeline:
                 name="tuple_dataset_to_torch_tensor_dataset"
         )
     ])
+
+def create_artificial_pipeline_no_shuffle(**kwargs) -> Pipeline:
+    return pipeline([
+        node(
+                func=conv_structure_to_lca_and_names,
+                inputs=["artificial_decay", "decay_tree_structure"],
+                outputs={
+                    "all_lca":"all_lca",
+                    "all_names":"all_names"
+                },
+                name="conv_structure_to_lca_and_names"
+        ),
+        node(
+                func=lca_and_leaves_sort_into_modes,
+                inputs=["artificial_decay", "all_lca", "all_names", "decay_tree_events"],
+                outputs={
+                    "all_lca_mode_sorted":"all_lca_mode_sorted",
+                    "all_leaves_mode_sorted":"all_leaves_mode_sorted"
+                },
+                name="lca_and_leaves_sort_into_modes"
+        ),
+        node(
+                func=shuffle_lca_and_leaves_in_mode,
+                inputs=["artificial_decay", "all_lca_mode_sorted", "all_leaves_mode_sorted"],
+                outputs={
+                    "all_lca_shuffled":"all_lca_shuffled",
+                    "all_leaves_shuffled":"all_leaves_shuffled"
+                },
+                name="shuffle_lca_and_leaves_in_mode"
+        ),
+        node(
+                func=lca_and_leaves_to_tuple_dataset,
+                inputs=["all_lca_shuffled", "all_leaves_shuffled"],
+                outputs={
+                    "dataset_lca_and_leaves":"dataset_lca_and_leaves"
+                },
+                name="lca_and_leaves_to_tuple_dataset"
+        ),
+        node(
+                func=tuple_dataset_to_torch_tensor_dataset,
+                inputs=["dataset_lca_and_leaves"],
+                outputs={
+                    "torch_dataset_lca_and_leaves":"torch_dataset_lca_and_leaves",
+                },
+                name="tuple_dataset_to_torch_tensor_dataset"
+        )
+    ])
