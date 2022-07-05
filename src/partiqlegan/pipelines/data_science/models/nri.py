6baa74cbc01d873e69fcc01cb46aa90fb67b0c2d
lc3267 <melvin.strobl@kit.edu>
2022-07-05 15:08:51 +0200

    comment

--- src/partiqlegan/pipelines/data_science/models/nri.py
+++ src/partiqlegan/pipelines/data_science/models/nri.py
@@ -3,129 +3,129 @@ from torch import Tensor, nn
 import torch
 import torch.nn.functional as F
 
-class NRIModel(nn.Module):
-    """Auto-encoder."""
-    def __init__(self, encoder: nn.Module, es: Tensor, size: int):
-        """
-        Args:
-            encoder: an encoder inferring relations
-            decoder: an decoder predicting future states
-            es: edge list
-            size: number of nodes
-        """
-        super(NRIModel, self).__init__()
-        self.enc = encoder
-        self.es = es
-        self.size = size
+# class NRIModel(nn.Module):
+#     """Auto-encoder."""
+#     def __init__(self, encoder: nn.Module, es: Tensor, size: int):
+#         """
+#         Args:
+#             encoder: an encoder inferring relations
+#             decoder: an decoder predicting future states
+#             es: edge list
+#             size: number of nodes
+#         """
+#         super(NRIModel, self).__init__()
+#         self.enc = encoder
+#         self.es = es
+#         self.size = size
 
-    def predict_relations(self, states: Tensor) -> Tensor:
-        """
-        Given historical node states, infer interacting relations.
+#     # def predict_relations(self, states: Tensor) -> Tensor:
+#     #     """
+#     #     Given historical node states, infer interacting relations.
 
-        Args:
-            states: [batch, step, node, dim]
+#     #     Args:
+#     #         states: [batch, step, node, dim]
 
-        Return:
-            prob: [E, batch, K]
-        """
-        # if not self.es.is_cuda:
-            # self.es = self.es.cuda(states.device)
-        logits = self.enc(states, self.es)
+#     #     Return:
+#     #         prob: [E, batch, K]
+#     #     """
+#     #     # if not self.es.is_cuda:
+#     #         # self.es = self.es.cuda(states.device)
+#     #     logits = self.enc(states, self.es)
 
-        return logits   # multi class ce loss takes logits rather than probs
+#     #     return logits   # multi class ce loss takes logits rather than probs
 
-        # prob = logits.softmax(-1)
-        # return prob
-
-
-def construct_rel_recvs(ln_leaves, self_interaction=False, device=None):
-    """
-    ln_leaves: list of ints, number of leaves for each sample in the batch
-    """
-    pad_len = max(ln_leaves)
-    rel_recvs = []
-    for l in ln_leaves:
-        rel_recv = torch.eye(pad_len, device=device)  # (l, l)
-        rel_recv[:, l:] = 0
-        rel_recv = rel_recv.repeat_interleave(pad_len, dim=1).T  # (l*l, l)
-        for j in range(l, pad_len):  # remove padding vertex edges TODO optimize
-            rel_recv[j::pad_len] = 0
-
-        if self_interaction == False:
-            rel_recv[0::pad_len + 1] = 0
-
-        rel_recvs.append(rel_recv)
-
-    return torch.stack(rel_recvs)
+#     #     # prob = logits.softmax(-1)
+#     #     # return prob
 
 
-def construct_rel_sends(ln_leaves, self_interaction=False, device=None):
-    """
-    ln_leaves: list of ints, number of leaves for each sample in the batch
-    """
-    pad_len = max(ln_leaves)
-    rel_sends = []
-    for l in ln_leaves:
-        rel_send = torch.eye(pad_len, device=device).repeat(pad_len, 1)
-        if self_interaction == False:
-            rel_send[torch.arange(0, pad_len*pad_len, pad_len+1)] = 0
-            # rel_send = rel_send[rel_send.sum(dim=1) > 0]  # (l*l, l)
+# def construct_rel_recvs(ln_leaves, self_interaction=False, device=None):
+#     """
+#     ln_leaves: list of ints, number of leaves for each sample in the batch
+#     """
+#     pad_len = max(ln_leaves)
+#     rel_recvs = []
+#     for l in ln_leaves:
+#         rel_recv = torch.eye(pad_len, device=device)  # (l, l)
+#         rel_recv[:, l:] = 0
+#         rel_recv = rel_recv.repeat_interleave(pad_len, dim=1).T  # (l*l, l)
+#         for j in range(l, pad_len):  # remove padding vertex edges TODO optimize
+#             rel_recv[j::pad_len] = 0
 
-        # padding
-        rel_send[:, l:] = 0
-        rel_send[l*(pad_len):] = 0
-        rel_sends.append(rel_send)
-    return torch.stack(rel_sends)
+#         if self_interaction == False:
+#             rel_recv[0::pad_len + 1] = 0
 
-def pad_collate_fn(batch):
-    ''' Collate function for batches with varying sized inputs
+#         rel_recvs.append(rel_recv)
 
-    This pads the batch with zeros to the size of the large sample in the batch
-
-    Args:
-        batch(tuple):  batch contains a list of tuples of structure (sequence, target)
-    Return:
-        (tuple): Input, labels, mask, all padded
-    '''
-    # First pad the input data
-    data = [item[0] for item in batch]
-    # Here we pad with 0 as it's the input, so need to indicate that the network ignores it
-    data = torch.nn.utils.rnn.pad_sequence(data, batch_first=True, padding_value=0.0)  # (N, L, F)
-    data = data.transpose(0, 1)  # (L, N, F)
-    # Then the labels
-    labels = [item[1] for item in batch]
-
-    # Note the -1 padding, this is where we tell the loss to ignore the outputs in those cells
-    target = torch.zeros(data.shape[1], data.shape[0], data.shape[0], dtype=torch.long) - 1  # (N, L, L)
-    # mask = torch.zeros(data.shape[0], data.shape[1], data.shape[1])  # (N, L, L)
-
-    # I don't know a cleaner way to do this, just copying data into the fixed-sized tensor
-    for i, tensor in enumerate(labels):
-        length = tensor.size(0)
-        target[i, :length, :length] = tensor
-        # mask[i, :length, :length] = 1
-
-    return data, target  # mask
+#     return torch.stack(rel_recvs)
 
 
-def rel_pad_collate_fn(batch, self_interaction=False):
-    ''' Collate function for batches with varying sized inputs
+# def construct_rel_sends(ln_leaves, self_interaction=False, device=None):
+#     """
+#     ln_leaves: list of ints, number of leaves for each sample in the batch
+#     """
+#     pad_len = max(ln_leaves)
+#     rel_sends = []
+#     for l in ln_leaves:
+#         rel_send = torch.eye(pad_len, device=device).repeat(pad_len, 1)
+#         if self_interaction == False:
+#             rel_send[torch.arange(0, pad_len*pad_len, pad_len+1)] = 0
+#             # rel_send = rel_send[rel_send.sum(dim=1) > 0]  # (l*l, l)
 
-    This pads the batch with zeros to the size of the large sample in the batch
+#         # padding
+#         rel_send[:, l:] = 0
+#         rel_send[l*(pad_len):] = 0
+#         rel_sends.append(rel_send)
+#     return torch.stack(rel_sends)
 
-    Args:
-        batch(tuple):  batch contains a list of tuples of structure (sequence, target)
-    Return:
-        (tuple): Input, labels, rel_rec, rel_send, all padded
-    '''
-    lens = [sample[0].size(0) for sample in batch]
+# def pad_collate_fn(batch):
+#     ''' Collate function for batches with varying sized inputs
 
-    data, target = pad_collate_fn(batch)
+#     This pads the batch with zeros to the size of the large sample in the batch
 
-    rel_recvs = construct_rel_recvs(lens, self_interaction=self_interaction)
-    rel_sends = construct_rel_sends(lens, self_interaction=self_interaction)
+#     Args:
+#         batch(tuple):  batch contains a list of tuples of structure (sequence, target)
+#     Return:
+#         (tuple): Input, labels, mask, all padded
+#     '''
+#     # First pad the input data
+#     data = [item[0] for item in batch]
+#     # Here we pad with 0 as it's the input, so need to indicate that the network ignores it
+#     data = torch.nn.utils.rnn.pad_sequence(data, batch_first=True, padding_value=0.0)  # (N, L, F)
+#     data = data.transpose(0, 1)  # (L, N, F)
+#     # Then the labels
+#     labels = [item[1] for item in batch]
 
-    return (data, rel_recvs, rel_sends), target
+#     # Note the -1 padding, this is where we tell the loss to ignore the outputs in those cells
+#     target = torch.zeros(data.shape[1], data.shape[0], data.shape[0], dtype=torch.long) - 1  # (N, L, L)
+#     # mask = torch.zeros(data.shape[0], data.shape[1], data.shape[1])  # (N, L, L)
+
+#     # I don't know a cleaner way to do this, just copying data into the fixed-sized tensor
+#     for i, tensor in enumerate(labels):
+#         length = tensor.size(0)
+#         target[i, :length, :length] = tensor
+#         # mask[i, :length, :length] = 1
+
+#     return data, target  # mask
+
+
+# def rel_pad_collate_fn(batch, self_interaction=False):
+#     ''' Collate function for batches with varying sized inputs
+
+#     This pads the batch with zeros to the size of the large sample in the batch
+
+#     Args:
+#         batch(tuple):  batch contains a list of tuples of structure (sequence, target)
+#     Return:
+#         (tuple): Input, labels, rel_rec, rel_send, all padded
+#     '''
+#     lens = [sample[0].size(0) for sample in batch]
+
+#     data, target = pad_collate_fn(batch)
+
+#     rel_recvs = construct_rel_recvs(lens, self_interaction=self_interaction)
+#     rel_sends = construct_rel_sends(lens, self_interaction=self_interaction)
+
+#     return (data, rel_recvs, rel_sends), target
     
 class MLP(nn.Module):
     """Two-layer fully-connected ELU net with batch norm."""
