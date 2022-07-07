bc7a560003ce1455494a9ffa9a4a70b24909cfc7
lc3267 <melvin.strobl@kit.edu>
2022-07-07 15:01:49 +0200

    cleanup of parameters

--- src/partiqlegan/pipelines/data_science/pipeline.py
+++ src/partiqlegan/pipelines/data_science/pipeline.py
@@ -12,9 +12,23 @@ def create_training_qgnn_pipeline(**kwargs) -> Pipeline:
     return pipeline([
         node(
                 func=train_qgnn,
-                inputs=["torch_dataset_lca_and_leaves", "params:n_hid", "params:n_momenta", "params:dropout_rate", "params:learning_rate", "params:learning_rate_decay", "params:gamma", "params:batch_size", "params:epochs"],
+                inputs={
+                        "torch_dataset_lca_and_leaves":"torch_dataset_lca_and_leaves",
+                        "n_momenta":"params:n_momenta",
+                        "n_blocks":"params:n_blocks",
+                        "dim_feedforward":"params:dim_feedforward",
+                        "n_layers_mlp":"params:n_layers_mlp",
+                        "n_additional_mlp_layers":"params:n_additional_mlp_layers",
+                        "n_final_mlp_layers":"params:n_final_mlp_layers",
+                        "dropout_rate":"params:dropout_rate",
+                        "learning_rate":"params:learning_rate",
+                        "learning_rate_decay":"params:learning_rate_decay",
+                        "gamma":"params:gamma",
+                        "batch_size":"params:batch_size",
+                        "epochs":"params:epochs"
+                    },
                 outputs={
-                    "model_qgnn":"model_qgnn"
+                        "model_qgnn":"model_qgnn"
                 },
                 name="train_qgnn"
         )
