6540e4727db2c42b2167cbde4fb0b96000c69573
lc3267 <melvin.strobl@kit.edu>
2022-07-08 11:25:19 +0200

    more convenient import but not sure if thats necessary at all

--- src/partiqlegan/pipelines/data_generation/__init__.py
+++ src/partiqlegan/pipelines/data_generation/__init__.py
@@ -3,6 +3,6 @@ This is a boilerplate pipeline 'data_generation'
 generated using Kedro 0.17.7
 """
 
-from .pipeline import create_artificial_pipeline, create_belleII_pipeline
+from .pipeline import *
 
 __all__ = ["create_artificial_pipeline", "create_belleII_pipeline"]
--- src/partiqlegan/pipelines/data_science/__init__.py
+++ src/partiqlegan/pipelines/data_science/__init__.py
@@ -3,6 +3,6 @@ This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.17.7
 """
 
-from .pipeline import create_training_qgnn_pipeline
+from .pipeline import *
 
-__all__ = ["create_pipeline"]
+__all__ = ["create_training_qgnn_pipeline, create_training_qgnn_pipeline_no_param_log"]
