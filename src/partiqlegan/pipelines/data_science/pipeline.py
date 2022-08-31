54215888ee2592a19b3cbd8b4d52cb8060ad6302
lc3267 <melvin.strobl@kit.edu>
2022-08-31 15:52:15 +0200

    optuna pipeline added

--- src/partiqlegan/pipelines/data_science/pipeline.py
+++ src/partiqlegan/pipelines/data_science/pipeline.py
@@ -52,6 +52,7 @@ def create_training_qgnn_pipeline(**kwargs) -> Pipeline:
                     "batchnorm": "params:batchnorm",
                     "symmetrize": "params:symmetrize",
                     "data_reupload": "params:data_reupload",
+                    "add_rot_gates": "params:add_rot_gates",
                     "n_fsps": "n_fsps",
                     "device": "params:device",
                 },
@@ -86,10 +87,87 @@ def create_training_qgnn_pipeline(**kwargs) -> Pipeline:
         ]
     )
 
+def create_training_optuna_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            node(
+                func=log_git_repo,
+                inputs={"git_hash_identifier": "params:git_hash_identifier"},
+                outputs={},
+                name="log_git_repo",
+            ),
+            node(
+                func=calculate_n_classes,
+                inputs={
+                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
+                },
+                outputs={"n_classes": "n_classes"},
+                name="calculate_n_classes",
+            ),
+            node(
+                func=calculate_n_fsps,
+                inputs={
+                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
+                },
+                outputs={"n_fsps": "n_fsps"},
+                name="calculate_n_fsps",
+                tags="split_run",
+            ),
+            node(
+                func=create_hyperparam_optimizer,
+                inputs={
+                    "n_classes": "n_classes",
+                    "n_momenta": "params:n_momenta",
+                    "model_sel": "params:model_sel",
+                    "n_blocks_range": "params:n_blocks_range",
+                    "dim_feedforward_range": "params:dim_feedforward_range",
+                    "n_layers_mlp_range": "params:n_layers_mlp_range",
+                    "n_additional_mlp_layers_range": "params:n_additional_mlp_layers_range",
+                    "n_final_mlp_layers_range": "params:n_final_mlp_layers_range",
+                    "dropout_rate_range": "params:dropout_rate_range",
+                    "factor": "params:factor",
+                    "tokenize": "params:tokenize",
+                    "embedding_dims": "params:embedding_dims",
+                    "batchnorm": "params:batchnorm",
+                    "symmetrize": "params:symmetrize",
+                    "data_reupload_range": "params:data_reupload_range",
+                    "n_fsps": "n_fsps",
+                    "device": "params:device",
+
+                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
+                    "learning_rate_range": "params:learning_rate_range",
+                    "learning_rate_decay_range": "params:learning_rate_decay_range",
+                    "gamma": "params:gamma",
+                    "batch_size_range": "params:batch_size_range",
+                    "epochs": "params:epochs",
+                    "normalize": "params:normalize",
+                    "plot_mode": "params:plot_mode",
+                    "detectAnomaly": "params:detect_anomaly",
+                    "redis_host": "params:redis_host",
+                    "redis_port": "params:redis_port",
+                    "redis_path": "params:redis_path",
+                    "redis_password": "params:redis_password"
+                },
+                outputs={"hyperparam_optimizer": "hyperparam_optimizer"},
+                name="create_instructor",
+            ),
+            node(
+                func=train_optuna,
+                inputs={"hyperparam_optimizer": "hyperparam_optimizer"},
+                outputs={"trained_model": "trained_model", "gradients": "gradients"},
+                name="train_optuna",
+            ),
+        ]
+    )
+
+
 
 # additional pipeline just to add it as an exception in kedro
 def create_debug_training_qgnn_pipeline(**kwargs) -> Pipeline:
     return create_training_qgnn_pipeline(**kwargs)
+    
+def create_debug_training_optuna_pipeline(**kwargs) -> Pipeline:
+    return create_training_optuna_pipeline(**kwargs)
 
 
 def create_split_training_qgnn_pipeline(**kwargs) -> Pipeline:
@@ -139,6 +217,7 @@ def create_split_training_qgnn_pipeline(**kwargs) -> Pipeline:
                     "symmetrize": "params:symmetrize",
                     "pre_trained_model": "trained_model",
                     "data_reupload": "params:data_reupload",
+                    "add_rot_gates": "params:add_rot_gates",
                     "n_fsps": "n_fsps",
                     "device": "params:device",
                 },
