cd1e7ebff26c1a79955a3e06b7de2e27f7ffb5ad
lc3267 <melvin.strobl@kit.edu>
2022-09-26 22:10:41 +0200

    cleanup

--- src/partiqlegan/pipelines/data_science/pipeline.py
+++ src/partiqlegan/pipelines/data_science/pipeline.py
@@ -7,10 +7,8 @@ from kedro.pipeline import Pipeline, node, pipeline
 
 from .nodes import *
 
-
-def create_training_qgnn_pipeline(**kwargs) -> Pipeline:
-    return pipeline(
-        [
+def create_metadata_nodes(**kwargs) -> Pipeline:
+    return [
             node(
                 func=log_git_repo,
                 inputs={"git_hash_identifier": "params:git_hash_identifier"},
@@ -34,6 +32,12 @@ def create_training_qgnn_pipeline(**kwargs) -> Pipeline:
                 name="calculate_n_fsps",
                 tags="split_run",
             ),
+        ]
+
+def create_training_qgnn_pipeline(**kwargs) -> Pipeline:
+    return pipeline(
+        [
+            *create_metadata_nodes(**kwargs),
             node(
                 func=create_model,
                 inputs={
@@ -95,29 +99,7 @@ def create_training_qgnn_pipeline(**kwargs) -> Pipeline:
 def create_training_optuna_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-            node(
-                func=log_git_repo,
-                inputs={"git_hash_identifier": "params:git_hash_identifier"},
-                outputs={},
-                name="log_git_repo",
-            ),
-            node(
-                func=calculate_n_classes,
-                inputs={
-                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
-                },
-                outputs={"n_classes": "n_classes"},
-                name="calculate_n_classes",
-            ),
-            node(
-                func=calculate_n_fsps,
-                inputs={
-                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
-                },
-                outputs={"n_fsps": "n_fsps"},
-                name="calculate_n_fsps",
-                tags="split_run",
-            ),
+            *create_metadata_nodes(**kwargs),
             node(
                 func=create_hyperparam_optimizer,
                 inputs={
@@ -169,48 +151,17 @@ def create_training_optuna_pipeline(**kwargs) -> Pipeline:
         ]
     )
 
-
-
-# additional pipeline just to add it as an exception in kedro
-def create_debug_training_qgnn_pipeline(**kwargs) -> Pipeline:
-    return create_training_qgnn_pipeline(**kwargs)
-    
-def create_debug_training_optuna_pipeline(**kwargs) -> Pipeline:
-    return create_training_optuna_pipeline(**kwargs)
-
-def resume_training_qgnn_pipeline(**kwargs) -> Pipeline:
+def create_resume_training_qgnn_pipeline(**kwargs) -> Pipeline:
     return pipeline(
         [
-            node(
-                func=log_git_repo,
-                inputs={"git_hash_identifier": "params:git_hash_identifier"},
-                outputs={},
-                name="log_git_repo",
-            ),
-            node(
-                func=calculate_n_classes,
-                inputs={
-                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
-                },
-                outputs={"n_classes": "n_classes"},
-                name="calculate_n_classes",
-            ),
-            node(
-                func=calculate_n_fsps,
-                inputs={
-                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
-                },
-                outputs={"n_fsps": "n_fsps"},
-                name="calculate_n_fsps",
-                tags="split_run",
-            ),
+            *create_metadata_nodes(**kwargs),
             node(
                 func=unpack_checkpoint,
                 inputs={
-                    "checkpoint": checkpoint,
+                    "checkpoint": "checkpoint",
                 },
-                outputs={"n_fsps": "n_fsps"},
-                name="calculate_n_fsps",
+                outputs={"model_state_dict": "model_state_dict", "optimizer_state_dict": "optimizer_state_dict", "start_epoch":"start_epoch"},
+                name="unpack_checkpoint",
                 tags="split_run",
             ),
             node(
@@ -262,10 +213,10 @@ def resume_training_qgnn_pipeline(**kwargs) -> Pipeline:
             ),
             node(
                 func=train,
-                inputs={"instructor": "instructor", "start_epoch": start_epoch},
+                inputs={"instructor": "instructor", "start_epoch": "start_epoch"},
                 outputs={
                     "trained_model": "trained_quantum_model",
-                    "checkpoint": "checkpoint",
+                    "checkpoint": "checkpoint_out",
                     "gradients": "gradients",
                 },
                 name="train_qgnn",
@@ -273,94 +224,102 @@ def resume_training_qgnn_pipeline(**kwargs) -> Pipeline:
         ]
     )
 
-def create_split_training_qgnn_pipeline(**kwargs) -> Pipeline:
-    return pipeline(
-        [
-            node(
-                func=log_git_repo,
-                inputs={"git_hash_identifier": "params:git_hash_identifier"},
-                outputs={},
-                name="log_git_repo",
-                tags="split_run",
-            ),
-            node(
-                func=calculate_n_classes,
-                inputs={
-                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
-                },
-                outputs={"n_classes": "n_classes"},
-                name="calculate_n_classes",
-                tags="split_run",
-            ),
-            node(
-                func=calculate_n_fsps,
-                inputs={
-                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
-                },
-                outputs={"n_fsps": "n_fsps"},
-                name="calculate_n_fsps",
-                tags="split_run",
-            ),
-            node(
-                func=create_model,
-                inputs={
-                    "n_classes": "n_classes",
-                    "n_momenta": "params:n_momenta",
-                    "model_sel": "params:post_model_sel",
-                    "n_blocks": "params:n_blocks",
-                    "dim_feedforward": "params:dim_feedforward",
-                    "n_layers_mlp": "params:n_layers_mlp",
-                    "n_additional_mlp_layers": "params:n_additional_mlp_layers",
-                    "n_final_mlp_layers": "params:n_final_mlp_layers",
-                    "dropout_rate": "params:dropout_rate",
-                    "factor": "params:factor",
-                    "tokenize": "params:tokenize",
-                    "embedding_dims": "params:embedding_dims",
-                    "batchnorm": "params:batchnorm",
-                    "symmetrize": "params:symmetrize",
-                    "pre_trained_model": "trained_model",
-                    "data_reupload": "params:data_reupload",
-                    "add_rot_gates": "params:add_rot_gates",
-                    "n_layers_vqc": "params:n_layers_vqc",
-                    "n_fsps": "n_fsps",
-                    "device": "params:device",
-                },
-                outputs={"nri_model": "quantum_model"},
-                name="create_quantum_model",
-                tags="split_run",
-            ),
-            node(
-                func=create_instructor,
-                inputs={
-                    "dataset_lca_and_leaves": "dataset_lca_and_leaves",
-                    "model": "quantum_model",
-                    "learning_rate": "params:learning_rate",
-                    "learning_rate_decay": "params:learning_rate_decay",
-                    "gamma": "params:gamma",
-                    "batch_size": "params:batch_size",
-                    "epochs": "params:epochs",
-                    "normalize": "params:normalize",
-                    "plot_mode": "params:plot_mode",
-                    "detectAnomaly": "params:detect_anomaly",
-                    "device": "params:device",
-                    "n_fsps": "n_fsps",
-                },
-                outputs={"instructor": "quantum_instructor"},
-                name="create_quantum_instructor",
-                tags="split_run",
-            ),
-            node(
-                func=train,
-                inputs={
-                    "instructor": "quantum_instructor",
-                },
-                outputs={
-                    "trained_model": "trained_quantum_model",
-                    "checkpoint": "checkpoint",
-                    "gradients": "gradients",
-                },
-                name="train_qgnn",
-                tags=["split_run", "training"],
-            ),
-        ]
-    )
+
+# additional pipeline just to add it as an exception in kedro
+def create_debug_training_qgnn_pipeline(**kwargs) -> Pipeline:
+    return create_training_qgnn_pipeline(**kwargs)
+    
+def create_debug_training_optuna_pipeline(**kwargs) -> Pipeline:
+    return create_training_optuna_pipeline(**kwargs)
+
+# def create_split_training_qgnn_pipeline(**kwargs) -> Pipeline:
+#     return pipeline(
+#         [
+#             node(
+#                 func=log_git_repo,
+#                 inputs={"git_hash_identifier": "params:git_hash_identifier"},
+#                 outputs={},
+#                 name="log_git_repo",
+#                 tags="split_run",
+#             ),
+#             node(
+#                 func=calculate_n_classes,
+#                 inputs={
+#                     "dataset_lca_and_leaves": "dataset_lca_and_leaves",
+#                 },
+#                 outputs={"n_classes": "n_classes"},
+#                 name="calculate_n_classes",
+#                 tags="split_run",
+#             ),
+#             node(
+#                 func=calculate_n_fsps,
+#                 inputs={
+#                     "dataset_lca_and_leaves": "dataset_lca_and_leaves",
+#                 },
+#                 outputs={"n_fsps": "n_fsps"},
+#                 name="calculate_n_fsps",
+#                 tags="split_run",
+#             ),
+#             node(
+#                 func=create_model,
+#                 inputs={
+#                     "n_classes": "n_classes",
+#                     "n_momenta": "params:n_momenta",
+#                     "model_sel": "params:post_model_sel",
+#                     "n_blocks": "params:n_blocks",
+#                     "dim_feedforward": "params:dim_feedforward",
+#                     "n_layers_mlp": "params:n_layers_mlp",
+#                     "n_additional_mlp_layers": "params:n_additional_mlp_layers",
+#                     "n_final_mlp_layers": "params:n_final_mlp_layers",
+#                     "dropout_rate": "params:dropout_rate",
+#                     "factor": "params:factor",
+#                     "tokenize": "params:tokenize",
+#                     "embedding_dims": "params:embedding_dims",
+#                     "batchnorm": "params:batchnorm",
+#                     "symmetrize": "params:symmetrize",
+#                     "pre_trained_model": "trained_model",
+#                     "data_reupload": "params:data_reupload",
+#                     "add_rot_gates": "params:add_rot_gates",
+#                     "n_layers_vqc": "params:n_layers_vqc",
+#                     "n_fsps": "n_fsps",
+#                     "device": "params:device",
+#                 },
+#                 outputs={"nri_model": "quantum_model"},
+#                 name="create_quantum_model",
+#                 tags="split_run",
+#             ),
+#             node(
+#                 func=create_instructor,
+#                 inputs={
+#                     "dataset_lca_and_leaves": "dataset_lca_and_leaves",
+#                     "model": "quantum_model",
+#                     "learning_rate": "params:learning_rate",
+#                     "learning_rate_decay": "params:learning_rate_decay",
+#                     "gamma": "params:gamma",
+#                     "batch_size": "params:batch_size",
+#                     "epochs": "params:epochs",
+#                     "normalize": "params:normalize",
+#                     "plot_mode": "params:plot_mode",
+#                     "detectAnomaly": "params:detect_anomaly",
+#                     "device": "params:device",
+#                     "n_fsps": "n_fsps",
+#                 },
+#                 outputs={"instructor": "quantum_instructor"},
+#                 name="create_quantum_instructor",
+#                 tags="split_run",
+#             ),
+#             node(
+#                 func=train,
+#                 inputs={
+#                     "instructor": "quantum_instructor",
+#                 },
+#                 outputs={
+#                     "trained_model": "trained_quantum_model",
+#                     "checkpoint": "checkpoint",
+#                     "gradients": "gradients",
+#                 },
+#                 name="train_qgnn",
+#                 tags=["split_run", "training"],
+#             ),
+#         ]
+#     )
