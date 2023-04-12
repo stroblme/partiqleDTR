3208383501b4708045f6145ccea8f2c66d7c1bfc
lc3267 <melvin.strobl@kit.edu>
2023-04-12 15:20:45 +0200

    mlflow merge script

---
+++ .vscode/mlflow_merge.py
@@ -0,0 +1,30 @@
+import glob
+import os
+import yaml
+import shutil
+
+mlflow_path = "./mlruns/1"
+
+runs = glob.glob(os.path.join(mlflow_path, "*"))
+
+for r in runs:
+    mark_for_update = False
+    if not os.path.isdir(r):
+        continue
+
+    with open(os.path.join(r, "meta.yaml"), "r") as f:
+        try:
+            content = yaml.safe_load(f)
+        except yaml.YAMLError as exc:
+            print(exc)
+        
+
+        artifact_uri = content["artifact_uri"]
+
+        if "local/scratch/mstrobl/partiqleDTR" in artifact_uri:
+            content["artifact_uri"] = artifact_uri.replace("local/scratch/mstrobl/partiqleDTR", "home/lc3267/Documents/CodeWorkspace/PartiqleGAN")
+            mark_for_update = True
+
+    if mark_for_update:
+        with open(os.path.join(r, "meta.yaml"), "w") as f:
+            yaml.safe_dump(content, f)
