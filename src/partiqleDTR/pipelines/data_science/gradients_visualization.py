0f2a6f791318e00d98498104311b2f8dc4805933
lc3267 <melvin.strobl@kit.edu>
2023-04-14 13:01:54 +0200

    fixed axis titles

--- src/partiqleDTR/pipelines/data_science/gradients_visualization.py
+++ src/partiqleDTR/pipelines/data_science/gradients_visualization.py
@@ -82,8 +82,9 @@ def heatmap_3d(
 
     fig.update_layout(
         title=dict(text=title),
-        yaxis=dict(title="Parameter Index"),
-        xaxis=dict(title="Epoch"),
+        yaxis_title="Parameter Index",
+        xaxis_title="Epoch",
+        zaxis_title="Gradient"
     )
 
     return fig
